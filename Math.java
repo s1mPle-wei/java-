4db5aaf034b8de3454fa513acb2e8d64cf28e643
s1mPle-wei <62126899+s1mPle-wei@users.noreply.github.com>
2020-12-10 21:06:02 +0800

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,27 @@
+package java第一次作业;
+
+public class Math {
+
+	public static void main(String[] args) {
+		System.out.println("常用的方法:"
+				+ "Math.round()    返回最接近参数的 int,它表示\"四舍五入\"\r\n" + 
+				"\r\n" + 
+				"                    Math.rint()         返回最接近参数并等于某一整数的 double 值，如果有2个数同样接近，则返回偶数的那个\r\n" + 
+				"\r\n" + 
+				"                    Math.floor()       返回最大的（最接近正无穷大）double 值，该值小于等于参数，并等于某个整数\r\n" + 
+				"\r\n" + 
+				"                    Math.ceil()         返回最小的（最接近负无穷大）double 值，该值大于等于参数，并等于某个整数\r\n" + 
+				"\r\n" + 
+				"                    Math.cbrt()        返回 double 值的立方根\r\n" + 
+				"\r\n" + 
+				"                    Math.sqrt()        返回正确舍入的 double 值的正平方根\r\n" + 
+				"\r\n" + 
+				"                    Math.pow()       返回第一个参数的第二个参数次幂的值\r\n" + 
+				"\r\n" + 
+				"                    Math.max()       返回两个 double 值中较大的一个\r\n" + 
+				"\r\n" + 
+				"                    Math.min()        返回两个 double 值中较小的一个");
+
+	}
+
+}
